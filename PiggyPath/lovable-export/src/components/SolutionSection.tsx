a71f571ee08c55d1bc67f8fb5d2708e7195de12f
Tanishkka27 <tanishkka27@gmail.com>
2026-02-16 21:39:26 +0530

    our vision content plus box updated

--- PiggyPath/lovable-export/src/components/SolutionSection.tsx
+++ PiggyPath/lovable-export/src/components/SolutionSection.tsx
@@ -3,7 +3,7 @@ const SolutionSection = () => {
     <section className="py-14 bg-background">
       
       <div className="container mx-auto px-4">
-        <div className="max-w-2xl mx-auto">
+        <div className="max-w-5xl mx-auto">
           
           <h2 className="text-2xl md:text-4xl font-bold text-center text-foreground mb-6">
             Our Vision
@@ -20,7 +20,7 @@ const SolutionSection = () => {
              We envision a generation that doesn't just survive adulthood, but owns it. By turning the mystery of finance into the fun of a game, we’re building a future where every young adult has the clarity to dream big and the tools to actually get there, no jargon, no judgment, just progress.
             </p>
             
-          </div>
+          </div><br /><br />
         </div>
       </div>
     </section>
